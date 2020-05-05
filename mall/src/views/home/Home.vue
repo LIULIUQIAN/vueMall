7a33b63c0015c7c784865f68294c45ee626a37d4
3284904004@qq.com <FFzf2016>
2020-05-05 10:59:18 +0800

    navbar

--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+	  <nav-bar class="nav-bar">
+		  <div slot="center">购物街</div>
+	  </nav-bar>
+  </div>
 </template>
 
 <script>
+	import NavBar from '../../components/common/navbar/NavBar.vue'
   export default {
-    name: "Home"
+    name: "Home",
+	components:{
+		NavBar
+	}
   }
 </script>
 
 <style scoped>
+	
+	.nav-bar{
+		background-color: var(--color-tint);
+		color: #FFFFFF;
+	}
 
 </style>
