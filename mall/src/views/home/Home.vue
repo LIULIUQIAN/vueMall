01a5a4114a30bd48c7d2d05c7e0f5318537e23da
3284904004@qq.com <FFzf2016>
2020-05-05 11:50:18 +0800

    banner

--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,26 +1,29 @@
 <template>
 	<div id="home">
 		<nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+		<home-swiper :banners="banners"></home-swiper>
 	</div>
 </template>
 
 <script>
 import NavBar from '../../components/common/navbar/NavBar.vue';
+import HomeSwiper from './childComps/HomeSwiper.vue'
 import {getHomeMultidata} from '../../network/home.js'
 export default {
 	name: 'Home',
 	data(){
 		return{
-			banner:[]
+			banners:[]
 		}
 	},
 	components: {
-		NavBar
+		NavBar,
+		HomeSwiper
 	},
 	created() {
 		getHomeMultidata().then(res=>{
 			console.log(res)
-			this.banner = res.data.banner.list;
+			this.banners = res.data.banner.list;
 			
 		})
 	}
