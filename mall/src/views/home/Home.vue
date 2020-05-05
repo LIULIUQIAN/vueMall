d24989af1df724eeb30fb22ab9e2104d5c39e2e7
3284904004@qq.com <FFzf2016>
2020-05-05 12:22:11 +0800

    推荐开发

--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -2,28 +2,33 @@
 	<div id="home">
 		<nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
 		<home-swiper :banners="banners"></home-swiper>
+		<recommend-view :recommends="recommends"></recommend-view>
 	</div>
 </template>
 
 <script>
 import NavBar from '../../components/common/navbar/NavBar.vue';
 import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/RecommendView.vue'
 import {getHomeMultidata} from '../../network/home.js'
 export default {
 	name: 'Home',
 	data(){
 		return{
-			banners:[]
+			banners:[],
+			recommends:[]
 		}
 	},
 	components: {
 		NavBar,
-		HomeSwiper
+		HomeSwiper,
+		RecommendView
 	},
 	created() {
 		getHomeMultidata().then(res=>{
 			console.log(res)
 			this.banners = res.data.banner.list;
+			this.recommends = res.data.recommend.list;
 			
 		})
 	}
---
+++ mall/src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="recommends">
+		<div class="recommends-item" v-for="item in recommends">
+			<a :href="item.link">
+				<img :src="item.image"/>
+				<div>{{item.title}}</div>
+			</a>
+		</div>
+	</div>
+</template>
+
+<script>
+	
+	export default{
+		name:'RecommendView',
+		props:{
+			recommends:{
+				default:[],
+				type:Array
+			}
+		},
+		components:{
+			
+		}
+	}
+</script>
+
+<style>
+	.recommends{
+		display: flex;
+		border-bottom: 10px solid #eee;
+		font-size: 14px;
+		margin-top: 10px;
+		padding-bottom: 20px;
+	}
+	.recommends-item{
+		flex: 1;
+		text-align: center;
+	}
+	.recommends img{
+		width: 70px;
+		height: 70px;
+		margin-bottom: 10px;
+	}
+</style>
