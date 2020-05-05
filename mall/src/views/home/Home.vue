f55e474d341fc01afdd79b75dc31ea09c271d328
3284904004@qq.com <FFzf2016>
2020-05-05 11:19:43 +0800

    接口封装

--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,26 +1,35 @@
 <template>
-  <div id="home">
-	  <nav-bar class="nav-bar">
-		  <div slot="center">购物街</div>
-	  </nav-bar>
-  </div>
+	<div id="home">
+		<nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+	</div>
 </template>
 
 <script>
-	import NavBar from '../../components/common/navbar/NavBar.vue'
-  export default {
-    name: "Home",
-	components:{
+import NavBar from '../../components/common/navbar/NavBar.vue';
+import {getHomeMultidata} from '../../network/home.js'
+export default {
+	name: 'Home',
+	data(){
+		return{
+			banner:[]
+		}
+	},
+	components: {
 		NavBar
+	},
+	created() {
+		getHomeMultidata().then(res=>{
+			console.log(res)
+			this.banner = res.data.banner.list;
+			
+		})
 	}
-  }
+};
 </script>
 
 <style scoped>
-	
-	.nav-bar{
-		background-color: var(--color-tint);
-		color: #FFFFFF;
-	}
-
+.nav-bar {
+	background-color: var(--color-tint);
+	color: #ffffff;
+}
 </style>
