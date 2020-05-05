1631cdce898d66e31670dcc0d7bf0778f410f852
3284904004@qq.com <FFzf2016>
2020-05-05 17:39:41 +0800

    首页数据展示

---
+++ mall/src/components/content/GoodsList/GoodsList.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="goods-list">
+		<goods-list-item v-for="(item,index) in goodsList" :goods="item"></goods-list-item>
+	</div>
+</template>
+
+<script>
+	import GoodsListItem from './GoodsListItem.vue'
+	export default{
+		components:{
+			GoodsListItem
+		},
+		data(){
+			return{
+				
+			}
+		},
+		props:{
+			goodsList:{
+				type:Array,
+				default:[]
+			}
+		}
+	}
+</script>
+
+<style>
+	.goods-list {
+	  display: flex;
+	  flex-wrap: wrap;
+	  justify-content: space-around;
+	  padding: 0px 5px;
+	}
+</style>
--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -1,13 +1,13 @@
 <template>
 	<div id="home">
 		<nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
-		<home-swiper :banners="banners"></home-swiper>
-		<recommend-view :recommends="recommends"></recommend-view>
-		<feature-view></feature-view>
-		<tab-control :titles="['流行', '新款', '精选']" class="tab-control"></tab-control>
-		
-		<div style="height: 500px;background-color: #666666;"></div>
-		<div style="height: 500px;background-color: #333333;"></div>
+		<scroll class="content">
+			<home-swiper :banners="banners"></home-swiper>
+			<recommend-view :recommends="recommends"></recommend-view>
+			<feature-view></feature-view>
+			<tab-control :titles="['流行', '新款', '精选']" class="tab-control" @itemClick="tabControlClick"></tab-control>
+			<goods-list :goodsList="showGoodsList"></goods-list>
+		</scroll>
 	</div>
 </template>
 
@@ -17,6 +17,8 @@ import HomeSwiper from './childComps/HomeSwiper.vue';
 import RecommendView from './childComps/RecommendView.vue';
 import FeatureView from './childComps/FeatureView.vue';
 import TabControl from '../../components/content/tabControl/TabControl.vue';
+import Scroll from '../../components/common/scroll/Scroll.vue'
+import GoodsList from '../../components/content/GoodsList/GoodsList.vue'
 import { getHomeMultidata ,getHomeData} from '../../network/home.js';
 export default {
 	name: 'Home',
@@ -29,6 +31,7 @@ export default {
 			  'new': {page: 1, list: []},
 			  'sell': {page: 1, list: []}
 			},
+			currentType:'pop'
 		};
 	},
 	components: {
@@ -36,7 +39,9 @@ export default {
 		HomeSwiper,
 		RecommendView,
 		FeatureView,
-		TabControl
+		TabControl,
+		Scroll,
+		GoodsList
 	},
 	created() {
 		this.getMultidata();
@@ -55,11 +60,28 @@ export default {
 		getData(type){
 			let page = this.goodsList[type].page;
 			getHomeData(type,page).then(res =>{
-				
-				console.log(res.data.list)
-				this.goodsList[type].list.push(res.data.list)
+				this.goodsList[type].list.push(...res.data.list)
 				this.goodsList[type].page += 1;
+				console.log(this.goodsList[type].list[0])
 			})
+		},
+		tabControlClick(index){
+			switch (index) {
+			  case 0:
+			    this.currentType = 'pop'
+			    break
+			  case 1:
+			    this.currentType = 'new'
+			    break
+			  case 2:
+			    this.currentType = 'sell'
+			    break
+			}
+		}
+	},
+	computed:{
+		showGoodsList(){
+			return this.goodsList[this.currentType].list
 		}
 	}
 };
@@ -67,7 +89,9 @@ export default {
 
 <style scoped>
 #home {
-	padding-top: 44px;
+	/* padding-top: 44px; */
+	/*position: relative;*/
+	height: 100vh;
 }
 .nav-bar {
 	background-color: var(--color-tint);
@@ -76,5 +100,13 @@ export default {
 .tab-control {
 	position: sticky;
 	top: 44px;
+}
+
+.content{
+	position: absolute;
+	top: 44px;
+	bottom: 49px;
+	left: 0;
+	right: 0;
 }
 </style>
