b6080836e0134a3df1a92e8537ce582536d5f360
3284904004@qq.com <FFzf2016>
2020-05-05 13:51:34 +0800

    TabControl

---
+++ mall/src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="tab-control">
+		<div v-for="(item, index) in titles" class="tab-control-item" 
+		:class="{ active: currentIndex == index }" @click="itemClick(index)">
+			<span>{{ item }}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TabControl',
+	data() {
+		return {
+			currentIndex: 0
+		};
+	},
+	props: {
+		titles: {
+			type: Array,
+			default: []
+		}
+	},
+	methods:{
+		itemClick(index){
+			this.currentIndex = index;
+		}
+	}
+};
+</script>
+
+<style>
+.tab-control {
+	display: flex;
+	height: 44px;
+	line-height: 44px;
+	background-color: #ffffff;
+}
+.tab-control-item {
+	flex: 1;
+	text-align: center;
+}
+.tab-control-item span {
+	padding: 5px;
+}
+.active {
+	color: var(--color-high-text);
+}
+.active span {
+	border-bottom: 3px solid var(--color-high-text);
+}
+</style>
--- mall/src/views/home/Home.vue
+++ mall/src/views/home/Home.vue
@@ -4,49 +4,55 @@
 		<home-swiper :banners="banners"></home-swiper>
 		<recommend-view :recommends="recommends"></recommend-view>
 		<feature-view></feature-view>
+		<tab-control :titles="['流行', '新款', '精选']" class="tab-control"></tab-control>
+		
+		<div style="height: 500px;background-color: #666666;"></div>
+		<div style="height: 500px;background-color: #333333;"></div>
 	</div>
 </template>
 
 <script>
 import NavBar from '../../components/common/navbar/NavBar.vue';
-import HomeSwiper from './childComps/HomeSwiper.vue'
-import RecommendView from './childComps/RecommendView.vue'
-import FeatureView from './childComps/FeatureView.vue'
-import {getHomeMultidata} from '../../network/home.js'
+import HomeSwiper from './childComps/HomeSwiper.vue';
+import RecommendView from './childComps/RecommendView.vue';
+import FeatureView from './childComps/FeatureView.vue';
+import TabControl from '../../components/content/tabControl/TabControl.vue';
+import { getHomeMultidata } from '../../network/home.js';
 export default {
 	name: 'Home',
-	data(){
-		return{
-			banners:[],
-			recommends:[]
-		}
+	data() {
+		return {
+			banners: [],
+			recommends: []
+		};
 	},
 	components: {
 		NavBar,
 		HomeSwiper,
 		RecommendView,
-		FeatureView
-		
+		FeatureView,
+		TabControl
 	},
 	created() {
-		getHomeMultidata().then(res=>{
-			console.log(res)
+		getHomeMultidata().then(res => {
+			console.log(res);
 			this.banners = res.data.banner.list;
 			this.recommends = res.data.recommend.list;
-			
-		})
+		});
 	}
 };
 </script>
 
 <style scoped>
-	#home{
-		padding-top: 44px;
-		
-		margin-bottom: 100px;
-	}
+#home {
+	padding-top: 44px;
+}
 .nav-bar {
 	background-color: var(--color-tint);
 	color: #ffffff;
+}
+.tab-control {
+	position: sticky;
+	top: 44px;
 }
 </style>
