1d4afb18d7855c05da67527f60c13216fdb07d4e
liuliuqian <1414231501@qq.com>
2020-05-19 10:46:34 +0800

    评论

--- mall/src/views/detail/childComps/DetailCommentInfo.vue
+++ mall/src/views/detail/childComps/DetailCommentInfo.vue
@@ -1,19 +1,113 @@
 <template>
-    <div>评论{{commentInfo}}}</div>
+    <div class="comment-info" v-if="Object.keys(commentInfo).length !== 0">
+        <div class="info-header">
+            <div class="header-title">用户评价</div>
+            <div class="header-more">
+                更多
+                <i class="arrow-right"/>
+            </div>
+        </div>
+
+        <div class="info-user">
+            <img :src="commentInfo.user.avatar">
+            <span>{{commentInfo.user.uname}}</span>
+        </div>
+        <div class="info-detail">
+            <p>{{commentInfo.content}}</p>
+            <div class="info-other">
+                <span class="date">{{commentInfo.created | showDate}}</span>
+                <span>{{commentInfo.style}}</span>
+            </div>
+            <div class="info-imgs">
+                <img :src="item" v-for="(item, index) in commentInfo.images">
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
+    import {formatDate} from "../../../commo/utils";
     export default {
         name: "DetailCommentInfo",
-        props:{
-            commentInfo:{
-                type:Object,
-                default:{}
+        props: {
+            commentInfo: {
+                type: Object,
+                default: {}
+            }
+        },
+        filters:{
+            showDate(value){
+                let date = new Date(value*1000);
+                return formatDate(date, 'yyyy-MM-dd hh:mm:ss')
             }
         }
     }
 </script>
 
 <style scoped>
+    .comment-info {
+        padding: 5px 12px;
+        color: #333333;
+        border-bottom: 5px solid #f2f5f8;
+    }
+
+    .info-header {
+        display: flex;
+        height: 50px;
+        border-bottom: 1px solid rgba(0, 0, 0, .1);
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .header-title {
+        font-size: 15px;
+    }
+
+    .header-more {
+        font-size: 13px;
+        margin-right: 10px;
+    }
+
+    .info-user {
+        padding: 10px 0 5px;
+        display: flex;
+        align-items: center;
+        font-size: 15px;
+    }
+
+    .info-user img {
+        width: 42px;
+        height: 42px;
+        border-radius: 50%;
+        margin-right: 10px;
+    }
+
+    .info-detail{
+        padding: 0 5px 15px;
+    }
+    .info-detail p{
+        font-size: 14px;
+        color: #777777;
+        line-height: 1.5;
+    }
+    .info-detail .info-other {
+        font-size: 12px;
+        color: #999;
+        margin-top: 10px;
+    }
+    .info-other .date {
+        margin-right: 8px;
+    }
+
+    .info-imgs {
+        margin-top: 10px;
+    }
+
+    .info-imgs img {
+        width: 70px;
+        height: 70px;
+        margin-right: 5px;
+    }
 
 </style>
